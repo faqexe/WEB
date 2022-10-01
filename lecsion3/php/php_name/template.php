756069a5d3c5ab2227dff5ae879e51e33a7d931c
faqexe <faqexe@icloud.com>
2022-10-01 14:34:18 +0300

    name

--- lecsion3/php/php_name/template.php
+++ lecsion3/php/php_name/template.php
@@ -10,30 +10,31 @@
 </head>
 
 <body>
-   <div class="my-form">
-      <h5>Задание 1. Реализовать систему которая будет здороваться по нажатию на Отправить</h5>
+   <h5>Задание 1. Реализовать систему которая будет здороваться по нажатию на Отправить</h5>
+   <div class="my-form container text-center w-25 border border-2 border-primary rounded">
       <form action="#">
          <div class="row">
             <div class="col">
                Как тебя зовут? =)
             </div>
          </div>
-         <div class="row">
-            <div class="col-2">
+         <div class="row justify-content-center">
+            <div class="col-2 col-md-auto w-75">
                <input class="form-control" type="text" name="fio">
             </div>
          </div>
          <div class="row mt-2">
             <div class="col">
-               <button type="submit" class="btn btn-primary">Отправить</button>
+               <button type="submit" class="btn btn-primary w-75">Отправить</button>
             </div>
          </div>
       </form>
+      <hr>
+      Привет, <?= $name ?>!
    </div>
-   <hr>
-   Привет, <?= $name ?>
 
    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+
 </body>
 
 </html>
