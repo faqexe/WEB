895705ea1a74d51671dc2efd406217fde05fabc2
faqexe <faqexe@icloud.com>
2022-10-06 12:33:42 +0300

    color blocks

--- lecsion3/php/php_color_block2/index.php
+++ lecsion3/php/php_color_block2/index.php
@@ -4,17 +4,40 @@ session_start();
 
 $color = $_GET['color'] ?? "";
 $type = $_GET['type'] ?? "";
+$block = $_GET['block'] ?? "";
 
 $bgColor = $_SESSION['bgColor'] ?? "";
 $fontColor = $_SESSION['fontColor'] ?? "";
 
-if ($type == 'Цвет фона') {
+$bgColor1 = $_SESSION['bgColor1'] ?? "";
+$fontColor1 = $_SESSION['fontColor1'] ?? "";
+
+$bgColor2 = $_SESSION['bgColor2'] ?? "";
+$fontColor2 = $_SESSION['fontColor2'] ?? "";
+
+if ($type == 'Цвет фона' and $block == 'block1') {
    $bgColor = $color;
    $_SESSION['bgColor'] = $bgColor;
 }
-if ($type == 'Цвет шрифта') {
+if ($type == 'Цвет шрифта' and $block == 'block1') {
    $fontColor = $color;
    $_SESSION['fontColor'] = $fontColor;
+}
+if ($type == 'Цвет фона' and $block == 'block2') {
+   $bgColor1 = $color;
+   $_SESSION['bgColor1'] = $bgColor1;
+}
+if ($type == 'Цвет шрифта' and $block == 'block2') {
+   $fontColor1 = $color;
+   $_SESSION['fontColor1'] = $fontColor1;
+}
+if ($type == 'Цвет фона' and $block == 'block3') {
+   $bgColor2 = $color;
+   $_SESSION['bgColor2'] = $bgColor2;
+}
+if ($type == 'Цвет шрифта' and $block == 'block3') {
+   $fontColor2 = $color;
+   $_SESSION['fontColor2'] = $fontColor2;
 }
 
 include "template.php";
--- lecsion3/php/php_color_block2/template.php
+++ lecsion3/php/php_color_block2/template.php
@@ -5,52 +5,51 @@
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>Document</title>
+   <title>Цветные блоки</title>
    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 </head>
 
 <body>
    <style>
       .container {
-         max-width: 800px;
-         min-height: 600px;
-         border: 2px solid black;
+         max-width: 700px;
+         min-height: 300px;
+      }
+
+      .block {
+         display: flex;
+         justify-content: center;
+         align-items: center;
       }
    </style>
    <h4>Задание 2. Написать приложение которое позволяет управлять цветом сразу трех блоков
    </h4>
-   <div class="container">
+   <div class="container border border-primary shadow text-center">
       <form action="#">
-         <div class="row mt-2">
-            <div class="col-3">
-               <input class="form-control mb-2 h-100" value="<?= $color ?>" type="color" name="color" id="">
+         <div class="row mt-3 justify-content-center ms-auto me-auto">
+            <div class="col-4 mb-1">
+               <input class="form-control mb-2 h-75" value="<?= $color ?>" type="color" name="color">
+               <div class="col-12">
+                  <select class="form-select" name="type">
+                     <option value="Цвет фона">Цвет фона</option>
+                     <option value="Цвет шрифта">Цвет шрифта</option>
+                  </select>
+                  <select class="form-select mt-2" name="block">
+                     <option value="block1">1</option>
+                     <option value="block2">2</option>
+                     <option value="block3">3</option>
+                  </select>
+               </div>
+               <button class="btn btn-primary w-100 mt-2" type="submit">Отправить</button>
             </div>
-            <div class="col-3 ms-2">
-               <select class="form-select" name="type">
-                  <option value="Цвет фона">Цвет фона</option>
-                  <option value="Цвет шрифта">Цвет шрифта</option>
-               </select>
-               <select class="form-select me-5 mt-2" name="block">
-                  <option value="block1">1</option>
-                  <option value="block2">2</option>
-                  <option value="block3">3</option>
-               </select>
-            </div>
-         </div>
-         <div class="row">
-            <div class="col mt-3">
-               <button class="btn btn-primary w-50" type="submit">Отправить</button>
-            </div>
-         </div>
-         <div class="row text-center mt-2">
-            <div class="col-3">
-               <div class="border border-dark p-2" style="background: <?= $bgColor ?>; color: <?= $fontColor ?>;">
+            <div class="col-5">
+               <div class="border border-dark h-50 block" style="background: <?= $bgColor ?>; color: <?= $fontColor ?>;">
                   <b style="font-size: 18px;">Текст</b>
                </div>
-               <div class="border border-dark p-2" style="background: <?= $bgColor ?>; color: <?= $fontColor ?>;">
+               <div class="border border-dark h-50 mt-2 block" style="background: <?= $bgColor1 ?>; color: <?= $fontColor1 ?>;">
                   <b style="font-size: 18px;">Текст</b>
                </div>
-               <div class="border border-dark p-2" style="background: <?= $bgColor ?>; color: <?= $fontColor ?>;">
+               <div class="border border-dark h-50 mt-2 block" style="background: <?= $bgColor2 ?>; color: <?= $fontColor2 ?>;">
                   <b style="font-size: 18px;">Текст</b>
                </div>
             </div>
